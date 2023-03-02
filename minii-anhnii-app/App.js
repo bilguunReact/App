0515d5f3d58e508de6cb1cc9f19ba69b231f6f35
bilgee0119 <bilgeeen416@gmail.com>
2023-03-02 23:29:01 +0800

    03/02

--- minii-anhnii-app/App.js
+++ minii-anhnii-app/App.js
@@ -3,50 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello, world!</Text>
-      <Text style={styles.subtitle}>This is a React Native design example.</Text>
-      <View style={styles.buttonContainer}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Button 1</Text>
-        </View>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Button 2</Text>
-        </View>
+      <View style={styles.container}>
+        <Text style={{color: "teal"}}> Миний анхны апп </Text>
+        <Text style={styles.myemoji}> Тавтай морил &#128526;</Text>
       </View>
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#39a0ca",
+    alignItems: "center",
+    justifyContent: "center"
   },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  subtitle: {
-    fontSize: 20,
-    marginBottom: 30,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-  },
-  button: {
-    backgroundColor: '#2196F3',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    marginHorizontal: 10,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
+  myemoji: { fontSize: 20 }
+})
