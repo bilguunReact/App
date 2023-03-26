329ee299ca3d8bd684fe292cd32e48ead838f9dd
bilgee0119 <bilgeeen416@gmail.com>
2023-03-26 18:07:02 +0800

    github update

--- minii-anhnii-app/App.js
+++ minii-anhnii-app/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Homescreen from "../minii-anhnii-app/screens/Hemoscreen";
+import Hoyrdahi from "./screens/Hoyrdahi";
+import Gurawdugaar from "./screens/Gurawdugaar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
-  return (
-      <View style={styles.container}>
-        <Text style={{color: "teal"}}> Миний анхны апп </Text>
-        <Text style={styles.myemoji}> Тавтай морил &#128526;</Text>
-      </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#39a0ca",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  myemoji: { fontSize: 20 }
-})
+const Stack = createNativeStackNavigator();
+  
+  export default function App() {
+    return (
+   <NavigationContainer>
+    <Stack.Navigator>
+        <Stack.Screen 
+            name="Home"
+            component={Homescreen}
+            options={{title: "Буцах"}}
+        />
+        <Stack.Screen
+          name="Second"
+          component={Hoyrdahi}
+        />
+        <Stack.Screen
+          name="Guravdahi"
+          component={Gurawdugaar}
+          options={{headerShown: false}}
+        />
+    </Stack.Navigator>
+   </NavigationContainer>
+    )
+};
+  
